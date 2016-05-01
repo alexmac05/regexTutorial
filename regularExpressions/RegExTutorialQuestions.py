ebe38242501f2d5b34a5ad00fdde108843e3a464
H. Alex McFerron <alexmac2010@gmail.com>
2016-05-01 10:46:42 -0700

    initial commit'

---
+++ regularExpressions/RegExTutorialQuestions.py
@@ -0,0 +1,36 @@
+#######################################################################################################################
+# STEP: ONE
+# TITLE: Do you have python working?
+# INSTRUCTIONS: uncomment the print statement and run it
+
+# THE CODE:
+#print("Hello pyladies oakland! Together we can do anything!")
+
+#######################################################################################################################
+# STEP: TWO
+# TITLE: An Example
+# INSTRUCTIONS: Make sure you have the file mbox-short.txt in the same directory as this python file, uncomment
+# the code and run it. This is an example to show you the power of regex. This file has a lot in it and this regex
+# (regular expression) extracts all the email addresses.
+
+# THE CODE:
+
+import re
+
+results = []
+
+hand = open('mbox-short.txt')
+
+for line in hand:
+    line = line.rstrip()
+    if re.search('From:', line):
+        print(line)
+
+    dictOfEmails = re.findall('From: \S+?@\S+',line)
+    print(dictOfEmails)
+
+    #results.append(dictOfEmails)
+
+print(results)
+
+######################################################################################################################
