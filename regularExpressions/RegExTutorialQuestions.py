e6d325c527133c93d531d733c9cad2ad03c51fe5
H. Alex McFerron <alexmac2010@gmail.com>
2016-05-01 19:32:24 -0700

    more problems

--- regularExpressions/RegExTutorialQuestions.py
+++ regularExpressions/RegExTutorialQuestions.py
@@ -145,19 +145,19 @@ import re
 # HINT: use cheatsheet! Use google. Also, there is more than one regex that will work.
 # THE CODE:
 
-inputString = r'Helaine McFerron' #Should pass
-inputStringTwo = r'Mary-Jo Harris'
-inputStringFails = r'alexmac2010@gmail.com' #should fail
+#inputString = r'Helaine McFerron' #Should pass
+#inputStringTwo = r'Mary-Jo Harris'
+#inputStringFails = r'alexmac2010@gmail.com' #should fail
 
 #regExString = r'YOU CREATE THE REGEX'
 
-m = re.search(regExString, inputStringTwo)
+#m = re.search(regExString, inputStringTwo)
 
-if m is not None:
-    print(m.group())
-    print(type(m))
-else:
-    print('m is None, so no match was found')
+#if m is not None:
+#    print(m.group())
+#    print(type(m))
+#else:
+#    print('m is None, so no match was found')
 
 
 #Answer
@@ -167,6 +167,88 @@ else:
 ######################################################################################################################
 
 
+#######################################################################################################################
+# STEP: eight
+# TITLE: Create a regular expression exercise
+# INSTRUCTIONS: Match any word and single letter separated by a comma and single space, as in last name, first
+# initial. Should pass: Garcia, B.
+# HINT: use cheatsheet! Use google. Also, there is more than one regex that will work.
+# THE CODE:
+
+#inputString = r'Garcia, B. ' #Should pass
+#inputStringFails = r'alexmac2010@gmail.com' #should fail
+
+#regExString = r'YOU CREATE THE REGEX'
+
+#m = re.search(regExString, inputString)
+
+#if m is not None:
+#    print(m.group())
+#    print(type(m))
+#else:
+#    print('m is None, so no match was found')
 
 
+#Answer
+#Possible answers
+#regExString = r'[A-Za-z-]+, [A-Za-z]'
+######################################################################################################################
+
+
+#######################################################################################################################
+# STEP: nine
+# TITLE: Demo - matching any single character with a dot .
+# INSTRUCTIONS: Play with the following example, until it is clear that a dot cannot match a \n
+# or a non-character (empty string) but it can match other characters. Also, what if you were looking
+# for 3.14. Understand all of these topics using this demo
+# Homework. To understand more about r'' you can read
+# http://stackoverflow.com/questions/2081640/what-exactly-do-u-and-r-string-flags-do-in-python-and-what-are-raw-string-l
+
+# THE CODE:
+
+#inputString1 = r'bend'
+#regExString1 = r'.end'
+#inputString2 = r'end'
+#inputString3 = '\nend' # do not use r'' with the newline character
+#print(inputString3) #notice it has a newline character '\n'
+#inputString4 = r"The end."
+
+#regExString2 = '3\.14'
+#inputString5 = '3.14'
+#inputString6 = '3P14'
+
+#m = re.match(regExString1, inputString3, re.DOTALL) #the re.DOTALL adds newlines to the list of excepted chars with .
+#m = re.match(regExString1, inputString1)
+#m = re.search(regExString1, inputString4)
+#m = re.match(regExString2, inputString6)
+
+
+#if m is not None:
+#    print(m.group())
+#    print(type(m))
+#else:
+#    print('m is None, so no match was found')
+
+
+######################################################################################################################
+
+
+#######################################################################################################################
+# STEP: ten
+# TITLE: Create a regular expression exercise
+# INSTRUCTIONS: Match simple web domain names that begin with "www." and end with a ".com" suffix;
+# for example, www.yahoo.com. Extra credit: If your regex also supports other high-level domain names,
+# such as .edu, .net, (for example www.foothill.edu)
+
+
+######################################################################################################################
+
+
+#######################################################################################################################
+# STEP: eleven
+# TITLE: Create a regular expression exercise
+# INSTRUCTIONS: 
+
+
+######################################################################################################################
 
